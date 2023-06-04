ea848760769eab35647ee80b895e3b429be1e727
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-06-04 16:07:59 +0000

    bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user["first_name"] = params["user"]["first_name"]
     @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
     redirect_to "/"
   end
