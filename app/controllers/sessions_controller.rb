67645104593c92ed431e427d20f6e9b57a17a051
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-06-04 16:13:49 +0000

    if login else

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by({ "email" => params["email"] })
+    if @user
+    
+    #may need to redirect somewhere else depending on what the login page is
+    else
+      redirect_to "/sessions/new"
+
+    
   end
 
   def destroy
