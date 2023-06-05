8f59ca09fb3458609d46330ed4b4488aff9ea7b2
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-06-05 16:59:48 +0000

    user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,20 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({ "email" => params["email"] })
     if @user
-    
-    #may need to redirect somewhere else depending on what the login page is
+      password_the_user_typed = params["password"]
+      password_in_the_database = @user["password"]
+      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed 
+        flash["notice"] = "You are logged in!"
+        redirect_to "/sessions"
+      #may need to redirect somewhere else depending on what the login page is
+      else
+        flash["notice"] = "Login failed"
+        redirect_to "/sessions/new"
+      end
     else
+      flash["notice"] = "Login failed"
       redirect_to "/sessions/new"
-
-    
+    end
   end
 
   def destroy
