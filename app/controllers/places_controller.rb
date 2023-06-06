0d4aa9b3400fbf9038936b373dde17cec0ada0c6
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-06-06 15:00:37 +0000

    places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,12 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @posts = Post.where({ "user_id" => @current_user["id"]})
+    end
+  end
+  def new
+    @place = Place.new
   end
 
   def new
