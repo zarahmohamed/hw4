af3761295cdfa9fb81d923dcc99828fa21085e99
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-06-06 15:06:22 +0000

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def new
+    @posts = Post.where({ "user_id" => @current_user["id"]})
     @post = Post.new
     @post.place_id = params["place_id"]
   end
@@ -11,6 +12,8 @@ class PostsController < ApplicationController
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
